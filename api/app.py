78e907bd52b612424199c5732a144f339c426c0d
minhduc0711 <phamminhduc0711@gmail.com>
2020-02-19 17:20:29 +0700

    create api for predict, get & post reviews

---
+++ api/app.py
@@ -0,0 +1,85 @@
+from flask import Flask, jsonify, request
+
+import torch
+import torch.nn.functional as F
+
+from . import db
+from ..ml.models import SentimentClassifier
+from ..ml.data import to_feature_vector, id_to_rating
+
+app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
+seq_len = 150
+all_chars = 'abcdefghijklmnopqrstuvwxyz0123456789!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ '
+
+weights_path = "../checkpoints/epoch_1.pt"
+model = SentimentClassifier(feature_dim=len(all_chars),
+                            seq_len=seq_len)
+if torch.cuda.is_available():
+    weights = torch.load(weights_path)
+else:
+    weights = torch.load(weights_path, map_location="cpu")
+model.load_state_dict(weights)
+model.eval()
+
+
+@app.route("/", methods=['GET'])
+def home():
+    return "server is running"
+
+
+@app.route("/review", methods=['GET'])
+def get_reviews():
+    query = db.Review.select()
+    return jsonify([r.serialize() for r in query])
+
+
+@app.route("/review", methods=['POST'])
+def post_review():
+    required_fields = [
+        "review",
+        "rating",
+        "suggested_rating",
+        "sentiment_score",
+        "brand",
+        "user_agent",
+        "ip_address",
+    ]
+    data = request.get_json()
+    for k in required_fields:
+        if k not in data.keys():
+            return jsonify({"error": f"'{k}' not found in body"})
+    query = db.Review.create(**data)
+    return jsonify(query.serialize())
+
+
+@app.route("/predict", methods=['POST'])
+def predict():
+    data = request.get_json()
+    if "review" not in data:
+        return jsonify({"error": "'review' not found in body"})
+    else:
+        rating, conf = predict_wrapper(data['review'])
+        print(f"predictions: {rating}, {conf}")
+        score = compute_dummy_score(rating, conf)
+        return jsonify({"score": score})
+
+
+def compute_dummy_score(rating, conf):
+    shift = {
+        "good": 2/3,
+        "average": 1/3,
+        "bad": 0,
+    }
+    return shift[rating] + (1/3) * conf if rating != "bad" \
+        else shift[rating] + (1/3) * (1 - conf)
+
+
+def predict_wrapper(review):
+    x = to_feature_vector(review, all_chars, seq_len)
+    x = x.unsqueeze(0)
+    with torch.no_grad():
+        scores = F.softmax(model(x), dim=1).squeeze()
+        conf, label_id = torch.max(scores, 0)
+    return id_to_rating(label_id.item()), conf.item()
---
+++ api/db.py
@@ -0,0 +1,38 @@
+import peewee as pw
+from . import config
+
+
+db = pw.PostgresqlDatabase(config.POSTGRES_DB,
+                           user=config.POSTGRES_USER,
+                           password=config.POSTGRES_PASSWORD,
+                           host=config.POSTGRES_HOST,
+                           port=config.POSTGRES_PORT)
+
+
+class Review(pw.Model):
+    review = pw.TextField()
+    rating = pw.IntegerField()
+    suggested_rating = pw.IntegerField()
+    sentiment_score = pw.FloatField()
+    brand = pw.TextField()
+    user_agent = pw.TextField()
+    ip_address = pw.TextField()
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'review': self.review,
+            'rating': int(self.rating),
+            'suggested_rating': int(self.suggested_rating),
+            'sentiment_score': self.sentiment_score,
+            'brand': self.brand,
+            'user_agent': self.user_agent,
+            'ip_address': self.ip_address,
+        }
+
+    class Meta:
+        database = db
+
+
+db.connect()
+db.create_tables([Review])
