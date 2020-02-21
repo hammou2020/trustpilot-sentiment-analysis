e133600101be4f5b6a54a12808771818a12829da
minhduc0711 <phamminhduc0711@gmail.com>
2020-02-21 10:51:40 +0700

    add batch norm

--- ml/models.py
+++ ml/models.py
@@ -8,7 +8,8 @@ class SentimentClassifier(nn.Module):
                  conv_num_kernels=[256] * 6,
                  conv_kernel_sizes=[7, 7, 3, 3, 3, 3],
                  pool_sizes=[3, 3, None, None, None, 3],
-                 fc_sizes=[1024, 1024, 3]):
+                 fc_sizes=[1024, 1024, 3],
+                 batchnorm=False):
         super(SentimentClassifier, self).__init__()
         self.feature_dim = feature_dim
         self.seq_len = seq_len
@@ -16,21 +17,32 @@ class SentimentClassifier(nn.Module):
         self.conv1ds = nn.ModuleList()
         self.pools = nn.ModuleList([None] * len(conv_num_kernels))
         self.fcs = nn.ModuleList()
+        self.batchnorm = batchnorm
+        if self.batchnorm:
+            self.bn_layers = nn.ModuleList()
 
         in_c = feature_dim
         for i in range(len(conv_num_kernels)):
             out_c = conv_num_kernels[i]
             kernel_size = conv_kernel_sizes[i]
             self.conv1ds.append(
-                nn.Conv1d(in_c, out_c, kernel_size))
-            in_c = out_c
+                nn.Conv1d(in_c, out_c, kernel_size, bias=not self.batchnorm))
+
+            if self.batchnorm:
+                self.bn_layers.append(nn.BatchNorm1d(out_c))
 
             if pool_sizes[i]:
                 self.pools[i] = nn.MaxPool1d(pool_sizes[i])
 
+            in_c = out_c
+
         in_feats = self._get_conv_output_flatten_size()
         for fc_size in fc_sizes:
             self.fcs.append(nn.Linear(in_feats, fc_size))
+
+            if self.batchnorm:
+                self.bn_layers.append(nn.BatchNorm1d(fc_size))
+
             in_feats = fc_size
         self.dropout = nn.Dropout(0.5)
 
@@ -39,6 +51,8 @@ class SentimentClassifier(nn.Module):
         for i, conv in enumerate(self.conv1ds):
             x = conv(x)
             x = F.relu(x)
+            if self.batchnorm:
+                x = self.bn_layers[i](x)
             if self.pools[i]:
                 x = self.pools[i](x)
             # print(x.shape)
@@ -48,8 +62,9 @@ class SentimentClassifier(nn.Module):
             x = fc(x)
             if i != len(self.fcs) - 1:
                 x = F.relu(x)
+                if self.batchnorm:
+                    x = self.bn_layers[i + len(self.conv1ds)](x)
                 x = self.dropout(x)
-#         x = F.softmax(x, dim=-1)
 
         return x
 
